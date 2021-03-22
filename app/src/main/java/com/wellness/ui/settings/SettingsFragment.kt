2cc02d970f29627a0a68377a64458a05d6e21622
Garin <garin.kra@gmail.com>
2021-03-22 07:19:59 +0700

    feat: add change language setting

--- app/src/main/java/com/wellness/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/wellness/ui/settings/SettingsFragment.kt
@@ -1,31 +1,37 @@
 package com.wellness.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.wellness.R
+import com.wellness.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), View.OnClickListener {
 
-    private lateinit var settingsViewModel: SettingsViewModel
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-                ViewModelProvider(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
-        return root
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.cardLanguageSettings.setOnClickListener(this)
+
+        return binding.root
+    }
+
+    override fun onClick(v: View?) {
+        if (v == binding.cardLanguageSettings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
